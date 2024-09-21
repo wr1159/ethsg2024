de93bbf5e3b06ca9e510770fab33640c7f816ba1
wr1159 <chuweiron18@gmail.com>
2024-09-21 13:19:22 +0800

    Frontend: fix did.json

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,30 +1,54 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    headers: async () => {
-        return [
-            {
-                source: "/.well-known/did.json",
-                headers: [
-                    {
-                        key: "Content-Type",
-                        value: "application/json",
-                    },
-                ],
-            },
-        ];
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "gateway.pinata.cloud",
-            },
-            {
-                protocol: "https",
-                hostname: "noun.pics",
-            },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "gateway.pinata.cloud",
+      },
+      {
+        protocol: "https",
+        hostname: "noun.pics",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
         ],
-    },
+      },
+    ];
+  },
 };
 
 export default nextConfig;

5f4ec3ca3b123d044646398a1c031a868d741eb0
wr1159 <chuweiron18@gmail.com>
2024-09-22 04:30:06 +0800

    Add theme switch button in navbar

--- frontend/components/navbar/index.tsx
+++ frontend/components/navbar/index.tsx
@@ -1,14 +1,16 @@
 // import { ModeToggle } from "./ModeToggle";
 
+import { ModeToggle } from "./ModeToggle";
+
 export function Navbar() {
     return (
         <header className="w-full py-4 flex justify-between items-center">
-            <div className="flex items-center">
+            <div className="flex items-center gap-x-4">
+                <ModeToggle />
                 <div className="hidden sm:inline text-xl font-bold">
                     Co(IN)munity
                 </div>
             </div>
-            {/* <ModeToggle /> */}
             <div className="flex items-center">
                 <w3m-button />
             </div>
